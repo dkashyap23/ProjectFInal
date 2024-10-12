46a2152973197dd069b9a1845d96b091bb85b8f8
dynamiceye01 <115204782+dynamiceye01@users.noreply.github.com>
2024-10-13 03:16:57 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -22,6 +22,33 @@ public class QuickSort
         // return pivot index
         return i;
     }
+    public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+    public static int partition1(int arr[], int low,int high){
+        int pivot = arr[high];
+        int i = low-1;
+        
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                // swap
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        // swap with pivot
+        i++;
+        arr[high] = arr[i];
+        arr[i] = pivot;
+        
+        // return pivot index
+        return i;
+    }
     public static void quickSort(int arr[],int low,int high){
         if(low<high){
             int pindx = partition(arr,low,high);
