57ccbdb68b98dcdd02d4f6d3e83d0ac5af8c4fcd
dynamiceye01 <115204782+dynamiceye01@users.noreply.github.com>
2024-10-13 03:18:27 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -4,6 +4,14 @@ class MergeSort {
 	// Merges two subarrays of arr[].
 	// First subarray is arr[l..m]
 	// Second subarray is arr[m+1..r]
+	public void display1() {
+    System.out.println("Method without parameter");
+  }
+
+  // method with single parameter
+  public void display2(int a) {
+    System.out.println("Method with a single parameter: " + a);
+  }
 	void merge(int arr[], int l, int m, int r)
 	{
 		// Find sizes of two subarrays to be merged
@@ -69,6 +77,12 @@ class MergeSort {
 			// Merge the sorted halves
 			merge(arr, l, m, r);
 		}
+	}static void printArraye(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
 	}
 
 	/* A utility function to print array of size n */
